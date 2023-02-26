afb75cd299a760c86696228282f4cc1f32a585d8
OzanAhmetDincer <ozanahmetdincer@gmail.com>
2023-02-26 21:42:23 +0300

    Düzenlemeler Devam

--- ETicaret.WebUI/Controllers/HomeController.cs
+++ ETicaret.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using ETicaret.WebUI.Models;
+﻿using ETicaret.Entities;
+using ETicaret.Service.Abstract;
+using ETicaret.WebUI.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -6,14 +8,36 @@ namespace ETicaret.WebUI.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
+        private readonly ISliderService _sliderService;
+        private readonly IBrandService _brandService;
+        private readonly IContactService _contactService;
+        private readonly IService<News> _newsService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IProductService productService, ICategoryService categoryService, ISliderService sliderService, IBrandService brandService, IContactService contactService, IService<News> newsService)
         {
-            _logger = logger;
+            _productService = productService;
+            _categoryService = categoryService;
+            _sliderService = sliderService;
+            _brandService = brandService;
+            _contactService = contactService;
+            _newsService = newsService;
         }
-
-        public IActionResult Index()
+        public async Task<IActionResult> IndexAsync()
+        {
+            var model = new HomePageViewModel()
+            {
+                Sliders = await _sliderService.GetAllAsync(),
+                Products = await _productService.GetAllAsync(),
+                Brands = await _brandService.GetAllAsync(),
+                Categories = await _categoryService.GetAllAsync(),
+                News = await _newsService.GetAllAsync()
+            };
+            return View(model);
+        }
+        [Route("AccessDenied")]
+        public IActionResult AccessDenied()
         {
             return View();
         }
---
+++ ETicaret.WebUI/Models/HomePageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ETicaret.Entities;
+
+namespace ETicaret.WebUI.Models
+{
+    public class HomePageViewModel
+    {
+        public List<Slider>? Sliders { get; set; }
+        public List<Product>? Products { get; set; }
+        public List<Brand>? Brands { get; set; }
+        public List<Category>? Categories { get; set; }
+        public List<News>? News { get; set; }
+
+    }
+}
